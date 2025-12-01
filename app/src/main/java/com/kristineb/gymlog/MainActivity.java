d5f4f20f38deb5ea4d2bd3104f8837f8cb1cbb72
kristine-barr <kristinebarruzo@gmail.com>
2025-12-01 13:55:33 -0800

    Video 11 Update login screen to use LiveData.

--- app/src/main/java/com/kristineb/gymlog/MainActivity.java
+++ app/src/main/java/com/kristineb/gymlog/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
@@ -14,6 +15,7 @@ import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LiveData;
 
 import com.kristineb.gymlog.database.GymLogRepository;
 import com.kristineb.gymlog.database.entities.GymLog;
@@ -25,6 +27,11 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     private static final String MAIN_ACTIVITY_USER_ID = "com.kristineb.gymlog.MAIN_ACTIVITY_USER_ID";
+
+    static final String SHARED_PREFERENCE_USERID_KEY = "com.kristineb.gymlog.SHARED_PREFERENCE_USERID_KEY";
+
+    static final String SHARED_PREFERENCE_USERID_VALUE = "com.kristineb.gymlog.SHARED_PREFERENCE_USERID_VALUE";
+    private static final int LOGGED_OUT = -1;
     private ActivityMainBinding binding;
 
     private GymLogRepository repository;
@@ -33,8 +40,6 @@ public class MainActivity extends AppCompatActivity {
     String mExercise = "";
     double mWeight = 0.0;
     int mReps = 0;
-
-    //TODO: Add login information
     private int loggedInUserId = -1;
 
     private User user;
@@ -47,8 +52,6 @@ public class MainActivity extends AppCompatActivity {
 
         // Make sure user is logged in
         loginUser();
-        // Make sure there is a user object
-        invalidateOptionsMenu();
 
         if(loggedInUserId == -1) {
             Intent intent = LoginActivity.loginIntentFactory(getApplicationContext());
@@ -78,11 +81,26 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
-
     private void loginUser() {
-        //TODO: Make Login method functional.
-        user = new User("Kristine", "password");
-        loggedInUserId = getIntent().getIntExtra(MAIN_ACTIVITY_USER_ID, -1);
+        // check shared preference for logged in user
+        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences(SHARED_PREFERENCE_USERID_KEY,
+                Context.MODE_PRIVATE);
+        loggedInUserId = sharedPreferences.getInt(SHARED_PREFERENCE_USERID_VALUE,LOGGED_OUT);
+        if(loggedInUserId != LOGGED_OUT) {
+            return;
+        }
+        //check intent for logged in user
+        loggedInUserId = getIntent().getIntExtra(MAIN_ACTIVITY_USER_ID, LOGGED_OUT);
+        if(loggedInUserId == LOGGED_OUT) {
+            return;
+        }
+        LiveData<User> userObserver = repository.getUserByUserId(loggedInUserId);
+        userObserver.observe(this, user -> {
+            if(user != null) {
+                invalidateOptionsMenu();
+            }
+        });
+
     }
 
     @Override
@@ -96,6 +114,11 @@ public class MainActivity extends AppCompatActivity {
     public boolean onPrepareOptionsMenu(Menu menu) {
         MenuItem item = menu.findItem(R.id.logoutMenuItem);
         item.setVisible(true);
+        if(user == null) {
+            return false;
+        }
+
+
         item.setTitle(user.getUsername());
         item.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
             @Override
@@ -135,7 +158,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void logout() {
-        //TODO: Finish logout method
+        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences(SHARED_PREFERENCE_USERID_KEY, Context.MODE_PRIVATE);
+        SharedPreferences.Editor sharedPrefEditor = sharedPreferences.edit();
+        sharedPrefEditor.putInt(SHARED_PREFERENCE_USERID_KEY, LOGGED_OUT);
+        sharedPrefEditor.apply();
+
+        getIntent().putExtra(MAIN_ACTIVITY_USER_ID, LOGGED_OUT);
         startActivity(LoginActivity.loginIntentFactory(getApplicationContext()));
     }
 
