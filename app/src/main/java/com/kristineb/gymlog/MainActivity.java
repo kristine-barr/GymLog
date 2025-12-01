a43e242f97e5582444563e7bb0b1defb8ca51761
kristine-barr <kristinebarruzo@gmail.com>
2025-12-01 11:46:50 -0800

    Video 10 Update login screen.

--- app/src/main/java/com/kristineb/gymlog/MainActivity.java
+++ app/src/main/java/com/kristineb/gymlog/MainActivity.java
@@ -11,7 +11,6 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -29,8 +28,6 @@ public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
 
     private GymLogRepository repository;
-
-    // global variables
 
     public static final String TAG = "KB_GYMLOG";
     String mExercise = "";
